07949f50e318c7051ada41bc363b1e9300295abe
pointcache <antonov.3d@gmail.com>
2017-02-14 01:01:38 -0500

    init

---
+++ Assets/rVar/CustomizeMe/CustomRVars.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/*
+[Serializable]
+public class r_Quality : rVar<Quality> {
+    public r_Quality() : base() { }
+    public r_Quality(Quality initialValue) : base(initialValue) { }
+    public static implicit operator Quality(r_Quality var) {
+        return var.Value;
+    }
+}
+*/
